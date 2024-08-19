3f234308bfc229eed763a49a3040045a245cc01a
kinyarasam <skinyara.30@gmail.com>
2024-08-19 16:51:50 +0300

    Project setup

---
+++ include/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#endif /** __MAIN_H__ */
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * Returns: Always 0 (success)
+ *          otherwise 1.
+ */
+int main(void) {
+    printf("Hello, World!\n");
+    return (0);
+}
